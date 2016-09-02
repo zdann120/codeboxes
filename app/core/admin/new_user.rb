11cee313533d61fe8541a2098556f58872a45ec5
Zach Nusbaum <nusbaumz@msu.edu>
2016-09-01 19:34:16 -0700

    add a Users link for admins

--- app/core/admin/new_user.rb
+++ app/core/admin/new_user.rb
@@ -6,6 +6,7 @@ class Admin::NewUser < ActiveInteraction::Base
   validates :password, presence: true
 
   def execute
+    return errors.add(:user, 'already exists') if User.find_by_email(email)
     user = User.new(email: email, password: password)
     errors.merge!(user.errors) unless user.save
     return user unless user.persisted?
