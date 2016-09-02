3c7eb4ea129cdc9f2abd8a164a82a7c7821b2608
Zach Nusbaum <nusbaumz@msu.edu>
2016-09-01 21:21:56 -0700

    destroy boxes when user destroyed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   devise :invitable, :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :boxes
+  has_many :boxes, dependent: :destroy
 
   def to_s
     email
