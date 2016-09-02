228ed52e88ed7783c300651a37472f857503796a
Zach Nusbaum <nusbaumz@msu.edu>
2016-09-01 19:44:16 -0700

    users must be created by an admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   before_create :set_token
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :boxes
