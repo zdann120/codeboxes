3f1ea22ff06a68ea0594f57a96e9bc4514a1611e
Zach Nusbaum <nusbaumz@msu.edu>
2016-09-06 14:55:58 -0700

    Send get request to /api/test with Authorization Header to decode the token.

--- app/controllers/api/snippets_controller.rb
+++ app/controllers/api/snippets_controller.rb
@@ -1,7 +1,7 @@
 class Api::SnippetsController < ApplicationController
   before_action :check_headers
   def test
-    # check_headers
+    render json: @result
   end
 
   def boxes
@@ -16,16 +16,11 @@ class Api::SnippetsController < ApplicationController
   def check_headers
     if request.headers['HTTP_AUTHORIZATION'].present?
       @token = request.headers['HTTP_AUTHORIZATION'].split(' ')[1].to_s
-      # pry
       @result = Tokens::Decode.run(token: @token).result
-
-      # render json:  @result[0]
     else
       render json: {msg: 'No authorization header found.'}
-      nil
     end
   rescue JWT::DecodeError
-    # render json: {error: 'Could not be decoded.'}
     render json: { error: 'An error occurred.' }
   end
 end
