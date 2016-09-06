fb036563ac98e64f70208ac673e567ca2292cd09
Zach Nusbaum <nusbaumz@msu.edu>
2016-09-05 20:15:11 -0700

    finishing touches

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -3,7 +3,7 @@ class JsonWebToken
     JWT.encode(payload, Rails.application.secrets.secret_key_base)
   end
 
-  def self.decode
+  def self.decode(token)
     return HashWithIndifferentAccess.new(JWT.decode(token, Rails.application.secrets.secret_key_base)[0])
   rescue
     nil
